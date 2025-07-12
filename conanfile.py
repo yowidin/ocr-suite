7dced77c8814ce60aea5db66fe2dcec8377379c6
YoWiDiN <yowidin@gmail.com>
2025-07-12 10:17:19 +0200

    Update dependencies
    - Replaced zlib with a newer version, otherwise build fails on macOS.
    - Bumped up some of the other dependencies.

--- conanfile.py
+++ conanfile.py
@@ -16,15 +16,15 @@ class Recipe(ConanFile):
 
     requires = [
         # Main app
-        'ffmpeg/5.0',
-        'tesseract/5.2.0',
-        'libpng/1.6.39',
+        'ffmpeg/7.1.1',
+        'tesseract/5.5.0',
+        'libpng/1.6.50',
         'catch2/3.2.1',
         'spdlog/1.9.2',
         'lyra/1.6.1',
         'sqlite-burrito/0.2.2@conan-burrito/stable',
         'indicators/2.2',
-        'boost/1.80.0',
+        'boost/1.88.0',
 
         # Viewer UI
         'sdl/2.26.1',
@@ -38,6 +38,9 @@ class Recipe(ConanFile):
         # Override incompatible dependencies
         self.requires('openssl/1.1.1t', override=True)
         self.requires('xz_utils/5.4.0', override=True)
+
+        # The default zlib 1.2.13 (required by ffmpeg, boost, and other packets) is not buildable on macOS 15.5
+        self.requires('zlib/1.3.1', override=True)
 
     def configure(self):
         # JPEG and libx264 are breaking the build on Ubuntu
