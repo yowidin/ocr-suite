8bcdd915665e0d227fbb706ec33a2a0c8c8590c2
YoWiDiN <yowidin@gmail.com>
2025-08-12 09:19:18 +0200

    OCSW: Add TOML-based config loader
    This is a first step in making the file watcher more generic - don't
    focus that much on the OCR-suite binaries and allow calling any app
    on any file change.

---
+++ tools/ocs-watcher/src/ocsw/config.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from typing import Annotated
+
+from pydantic import BaseModel, ConfigDict, StringConstraints, FilePath, DirectoryPath, AfterValidator
+from annotated_types import MinLen, Interval
+
+import tomlkit
+
+# A file extension starts with '.', consists of words and digits and can be anywhere between 1 and 6 characters long
+type FileExtension = Annotated[str, StringConstraints(pattern=r'^\.(?:\w|\d){1,6}$')]
+
+type NonEmptyString = Annotated[str, MinLen(1)]
+
+INPUT_FILE_PLACEHOLDER = "%file_path%"
+
+# Same ase input file path but without the file extension
+INPUT_FILE_STEM_PLACEHOLDER = "%file_stem_path%"
+
+MIN_CHECK_FREQUENCY = 5
+MAX_CHECK_FREQUENCY = 60 * 8
+DEFAULT_CHECK_FREQUENCY = 10
+
+DEFAULT_LOCK_NAME = 'ocsw.lock'
+
+
+def validate_call_arguments(v: list[str]) -> list[str]:
+    if INPUT_FILE_PLACEHOLDER not in v:
+        raise ValueError('call_arguments should contain a file placeholder: %file_path%')
+    return v
+
+
+class Config(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    # The directory to recursively check the files in
+    watch_directory: DirectoryPath
+
+    # File extensions to be checked
+    file_extensions: Annotated[list[FileExtension], MinLen(1)]
+
+    # Application to be called on each file with matching extension
+    target_app: FilePath
+
+    # Arguments to be passed to the target app, use %file_path% as a placeholder for passing
+    # the file path found inside the watch directory.
+    call_arguments: Annotated[list[NonEmptyString], MinLen(1), AfterValidator(validate_call_arguments)]
+
+    # How frequently should all the files be rechecked.
+    # This means that if a frequency is specified, then the target app should be re-entrant.
+    checking_frequency: Annotated[int, Interval(ge=MIN_CHECK_FREQUENCY,
+                                                le=MAX_CHECK_FREQUENCY)] | None = DEFAULT_CHECK_FREQUENCY
+
+    # Lock file name: doesn't allow running multiple instances of the same application
+    lock_name: NonEmptyString | None = DEFAULT_LOCK_NAME
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.checking_frequency is None:
+            self.checking_frequency = DEFAULT_CHECK_FREQUENCY
+
+        if self.lock_name is None:
+            self.lock_name = DEFAULT_LOCK_NAME
+
+    @staticmethod
+    def from_toml_file(path: Path) -> 'Config':
+        file_contents = path.read_text()
+        as_toml = tomlkit.loads(file_contents)
+        as_dict = as_toml.unwrap()
+        result = Config.model_validate(as_dict)
+        return result
---
+++ tools/ocs-watcher/tests/test_config.py
@@ -0,0 +1,151 @@
+import pytest
+from pathlib import Path
+from tempfile import NamedTemporaryFile, mkstemp
+
+from pydantic import ValidationError
+
+from ocsw.config import Config, MIN_CHECK_FREQUENCY, MAX_CHECK_FREQUENCY, DEFAULT_CHECK_FREQUENCY
+from ocsw.config import DEFAULT_LOCK_NAME
+
+import os
+
+
+class ConfigPlaceholder:
+    def __init__(self):
+        fd, path = mkstemp()
+        os.close(fd)
+
+        self._tmp_file = path
+
+        self.watch_directory = Path(path).parent
+        self.file_extensions = ['.mkv', '.mp4', '.avi', '.mov']
+        self.target_app = self._tmp_file
+        self.call_arguments = ['-i', '%file_path%']
+        self.checking_frequency = 20
+        self.lock_name = 'test.lock'
+
+    def cleanup(self):
+        if os.path.exists(self._tmp_file):
+            os.remove(self._tmp_file)
+
+    def as_dict(self):
+        return {
+            'watch_directory': self.watch_directory,
+            'file_extensions': self.file_extensions,
+            'target_app': self.target_app,
+            'call_arguments': self.call_arguments,
+            'checking_frequency': self.checking_frequency,
+            'lock_name': self.lock_name,
+        }
+
+
+@pytest.fixture
+def valid_config():
+    config = ConfigPlaceholder()
+    yield config
+    config.cleanup()
+
+
+def test_config_happy_path(valid_config):
+    _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_invalid_watch_dir(valid_config):
+    valid_config.watch_directory = '/invalid'
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_empty_extensions_list(valid_config):
+    valid_config.file_extensions = []
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_invalid_extension(valid_config):
+    valid_config.file_extensions = ['mkv']
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_non_file_target_app(valid_config):
+    valid_config.target_app = valid_config.watch_directory
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_non_existing_target_app(valid_config):
+    valid_config.target_app = '/invalid-app'
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_empty_call_arguments_list(valid_config):
+    valid_config.call_arguments = []
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_call_arguments_list_missing_file_placeholder(valid_config):
+    valid_config.call_arguments = ['-i']
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_checking_frequency_min_frequency_is_acceptable(valid_config):
+    valid_config.checking_frequency = MIN_CHECK_FREQUENCY
+    _ = Config(**valid_config.as_dict())
+
+
+def test_config_checking_frequency_max_frequency_is_acceptable(valid_config):
+    valid_config.checking_frequency = MAX_CHECK_FREQUENCY
+    _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_checking_frequency_too_small(valid_config):
+    valid_config.checking_frequency = MIN_CHECK_FREQUENCY - 1
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_fails_on_checking_frequency_too_big(valid_config):
+    valid_config.checking_frequency = MAX_CHECK_FREQUENCY + 1
+    with pytest.raises(ValidationError):
+        _ = Config(**valid_config.as_dict())
+
+
+def test_config_missing_checking_frequency_is_default_frequency(valid_config):
+    valid_config.checking_frequency = None
+    cfg = Config(**valid_config.as_dict())
+    assert cfg.checking_frequency == DEFAULT_CHECK_FREQUENCY
+
+
+def test_config_missing_lock_name_is_default_lock_name(valid_config):
+    valid_config.lock_name = None
+    cfg = Config(**valid_config.as_dict())
+    assert cfg.lock_name == DEFAULT_LOCK_NAME
+
+
+def test_config_fails_with_extra_args(valid_config):
+    as_dict = valid_config.as_dict()
+    as_dict['dummy-extra'] = 42
+    with pytest.raises(ValidationError):
+        _ = Config(**as_dict)
+
+
+def test_config_toml_works_on_happy_path(valid_config):
+    with NamedTemporaryFile(mode='w', delete_on_close=False) as f:
+        f.write(f"""
+watch_directory="{valid_config.watch_directory}"
+file_extensions={repr(valid_config.file_extensions)}
+target_app="{valid_config.target_app}"
+call_arguments={repr(valid_config.call_arguments)}
+checking_frequency={valid_config.checking_frequency}
+lock_name="{valid_config.lock_name}"
+""")
+        f.close()
+        path = Path(f.name)
+        from_toml = Config.from_toml_file(path)
+
+        direct = Config(**valid_config.as_dict())
+        assert direct == from_toml
